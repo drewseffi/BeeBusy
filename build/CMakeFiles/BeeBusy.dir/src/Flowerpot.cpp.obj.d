bd1e3f49e5ba1745844c47bb290ec0408fe6c18f
drewseffi <drewsutherland23@gmail.com>
2026-03-27 17:12:25 +0000

    add gitignore

--- build/CMakeFiles/BeeBusy.dir/src/Flowerpot.cpp.obj.d
+++ build/CMakeFiles/BeeBusy.dir/src/Flowerpot.cpp.obj.d
@@ -3,10 +3,11 @@ CMakeFiles/BeeBusy.dir/src/Flowerpot.cpp.obj: \
  C:/Users/drews/dev/BeeBusy/include/Flowerpot.h \
  C:/Users/drews/dev/BeeBusy/include/GameObject.h \
  C:/Users/drews/dev/BeeBusy/include/raylib.h \
- C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdarg.h \
- C:/msys64/mingw64/include/stdarg.h \
- C:/msys64/mingw64/include/_mingw_stdarg.h \
- C:/msys64/mingw64/include/vadefs.h C:/msys64/mingw64/include/_mingw.h \
- C:/msys64/mingw64/include/_mingw_mac.h \
- C:/msys64/mingw64/include/_mingw_secapi.h \
- C:/msys64/mingw64/include/sdks/_mingw_ddk.h
+ C:/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h
