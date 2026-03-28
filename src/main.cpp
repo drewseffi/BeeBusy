f29c48101bf918cf88907c7ed9c9cc13d329ff7a
drewseffi <drewsutherland23@gmail.com>
2026-03-28 02:55:27 +0000

    feat: add game states for game over

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,15 @@ int main(void)
     bool debug = false;
     Texture2D bg = LoadTexture("assets/grass.png");
 
+    //--Set up game state--
+    enum GameState
+    {
+        PLAYING,
+        GAME_OVER
+    };
+
+    GameState gameState = PLAYING;
+
     //--Set managers and player controller--
     BeeManager beeManager(screenWidth, screenHeight, 2.0f);
     Player player(screenWidth / 2, screenHeight / 2, 200);
@@ -28,175 +37,229 @@ int main(void)
 
     Seed seed(screenWidth, screenHeight, potManager.pots);
 
+    //--Reset block for managers--
+    auto ResetGame = [&]()
+    {
+        beeManager = BeeManager(screenWidth, screenHeight, 2.0f);
+
+        player = Player(screenWidth / 2, screenHeight / 2, 200);
+
+        potManager = PotManager(screenWidth, screenHeight, 6);
+        potManager.SpawnInitialPots();
+
+        seed = Seed(screenWidth, screenHeight, potManager.pots);
+
+        gameState = PLAYING;
+    };
+
     //--Game loop--
     while (!WindowShouldClose())
     {
         float deltaTime = GetFrameTime();
 
-        if (IsKeyPressed(KEY_ESCAPE))
-        {
-            debug = !debug;
-        }
-
-        //--Update beeManager--
-        beeManager.Update(deltaTime);
-
-        //--Check for player collision with pots--
-        Vector2 oldPosition = player.position;
-
-        player.Update(deltaTime);
-
-        for (auto &pot : potManager.pots)
-        {
-            if (CheckCollisionRecs(player.GetCollider(), pot.GetPotCollider()))
-            {
-                player.position = oldPosition;
-
-                if (player.hasSeed && !pot.hasPlant)
-                {
-                    pot.hasPlant = true;
-                    player.hasSeed = false;
-                }
-            }
-        }
-
-        //--Check player collision with seeds--
-        if (CheckCollisionRecs(player.GetCollider(), seed.GetCollider()))
-        {
-            player.hasSeed = true;
-        }
-
-        //--Check for bee collision with pots--
-        for (auto &bee : beeManager.bees)
-        {
-            for (auto &pot : potManager.pots)
-            {
-                if (CheckCollisionRecs(bee.GetCollider(), pot.GetPotCollider()) && pot.hasPlant)
-                {
-                    pot.hasPlant = false;
-                    
-                    bee.movingRight = !bee.movingRight;
-                }
-                else if (CheckCollisionRecs(bee.GetCollider(), pot.GetPlantCollider()) && pot.hasPlant)
-                {
-                    pot.hasPlant = false;
-                    
-                    bee.movingRight = !bee.movingRight;
-                }
-            }
-        }
-
-        //--Draw loop--
         BeginDrawing();
-        DrawTexture(bg, 0, 0, WHITE);
 
-        //--Draw all pots--
-        potManager.Draw();
-
-        //--Draw entities--
-        seed.Draw();
-        player.Draw();
-        beeManager.Draw();
-
-        //--Show seed text above player--
-        int textWidth = MeasureText("You have a plant, hurry!", 20);
-
-        int textStartX = player.position.x - textWidth / 2;
-
-        if (player.hasSeed)
+        if (gameState == PLAYING)
         {
-            DrawText("You have a plant, hurry!", textStartX, player.position.y - 60, 20, RED);   
-        }
-
-        //--Draw debug tools--
-        if (debug)
-        {
-            //--Draw player collision--
-            Rectangle playerCol = player.GetCollider();
-
-            DrawRectangleLines(
-                (int)playerCol.x,
-                (int)playerCol.y,
-                (int)playerCol.width,
-                (int)playerCol.height,
-                RED
-            );
-
-            //--Draw bee collision--
-            for (auto bees : beeManager.bees)
+            if (IsKeyPressed(KEY_ESCAPE))
             {
-                Rectangle collisionBox = bees.GetCollider();
-
-                DrawRectangleLines(
-                    (int)collisionBox.x,
-                    (int)collisionBox.y,
-                    (int)collisionBox.width,
-                    (int)collisionBox.height,
-                    RED
-                );
+                debug = !debug;
             }
 
-            //--Draw pot collision--
+            //--Check lose condition--
+            int potCount = 0;
             for (auto &pot : potManager.pots)
             {
-                Rectangle collisionBox = pot.GetPotCollider();
-
-                DrawRectangleLines(
-                    (int)collisionBox.x,
-                    (int)collisionBox.y,
-                    (int)collisionBox.width,
-                    (int)collisionBox.height,
-                    RED
-                );
-
-                collisionBox = pot.GetPlantCollider();
-
-                DrawRectangleLines(
-                    (int)collisionBox.x,
-                    (int)collisionBox.y,
-                    (int)collisionBox.width,
-                    (int)collisionBox.height,
-                    RED
-                );
+                if (pot.hasPlant)
+                {
+                    potCount++;
+                }
             }
 
-            Rectangle seedCol = seed.GetCollider();
+            if (potCount == 0)
+            {
+                gameState = GAME_OVER;
+            }
 
-            DrawRectangleLines(
-                (int)seedCol.x,
-                (int)seedCol.y,
-                (int)seedCol.width,
-                (int)seedCol.height,
-                RED
-            );
+            //--Update beeManager--
+            beeManager.Update(deltaTime);
 
-            //--Draw FPS--
-            DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, BLACK);
+            //--Check for player collision with pots--
+            Vector2 oldPosition = player.position;
 
-            //--Draw pot counter--
-            DrawText(TextFormat("Pots: %i", potManager.pots.size()), 10, 30, 20, BLACK);
+            player.Update(deltaTime);
 
-            //--Draw bee counter--
-            DrawText(TextFormat("Bees: %i", beeManager.bees.size()), 10, 50, 20, BLACK);
+            for (auto &pot : potManager.pots)
+            {
+                if (CheckCollisionRecs(player.GetCollider(), pot.GetPotCollider()))
+                {
+                    player.position = oldPosition;
 
-            //--Draw time--
-            DrawText(TextFormat("Num bees passed: %i", beeManager.BeesPassed()), 10, 70, 20, BLACK);
+                    if (player.hasSeed && !pot.hasPlant)
+                    {
+                        pot.hasPlant = true;
+                        player.hasSeed = false;
+                    }
+                }
+            }
+
+            //--Check player collision with seeds--
+            if (CheckCollisionRecs(player.GetCollider(), seed.GetCollider()))
+            {
+                player.hasSeed = true;
+            }
+
+            //--Check for bee collision with pots--
+            for (auto &bee : beeManager.bees)
+            {
+                for (auto &pot : potManager.pots)
+                {
+                    if (CheckCollisionRecs(bee.GetCollider(), pot.GetPotCollider()) && pot.hasPlant)
+                    {
+                        pot.hasPlant = false;
+                        
+                        bee.movingRight = !bee.movingRight;
+                    }
+                    else if (CheckCollisionRecs(bee.GetCollider(), pot.GetPlantCollider()) && pot.hasPlant)
+                    {
+                        pot.hasPlant = false;
+                        
+                        bee.movingRight = !bee.movingRight;
+                    }
+                }
+            }
+
+            //--Draw loop--
+            DrawTexture(bg, 0, 0, WHITE);
+
+            //--Draw all pots--
+            potManager.Draw();
+
+            //--Draw entities--
+            seed.Draw();
+            player.Draw();
+            beeManager.Draw();
+
+            //--Show seed text above player--
+            int textWidth = MeasureText("You have a plant, hurry!", 20);
+
+            int textStartX = player.position.x - textWidth / 2;
+
+            if (player.hasSeed)
+            {
+                DrawText("You have a plant, hurry!", textStartX, player.position.y - 60, 20, RED);   
+            }
+
+            //--Draw debug tools--
+            if (debug)
+            {
+                //--Draw player collision--
+                Rectangle playerCol = player.GetCollider();
+
+                DrawRectangleLines(
+                    (int)playerCol.x,
+                    (int)playerCol.y,
+                    (int)playerCol.width,
+                    (int)playerCol.height,
+                    RED
+                );
+
+                //--Draw bee collision--
+                for (auto bees : beeManager.bees)
+                {
+                    Rectangle collisionBox = bees.GetCollider();
+
+                    DrawRectangleLines(
+                        (int)collisionBox.x,
+                        (int)collisionBox.y,
+                        (int)collisionBox.width,
+                        (int)collisionBox.height,
+                        RED
+                    );
+                }
+
+                //--Draw pot collision--
+                for (auto &pot : potManager.pots)
+                {
+                    Rectangle collisionBox = pot.GetPotCollider();
+
+                    DrawRectangleLines(
+                        (int)collisionBox.x,
+                        (int)collisionBox.y,
+                        (int)collisionBox.width,
+                        (int)collisionBox.height,
+                        RED
+                    );
+
+                    collisionBox = pot.GetPlantCollider();
+
+                    DrawRectangleLines(
+                        (int)collisionBox.x,
+                        (int)collisionBox.y,
+                        (int)collisionBox.width,
+                        (int)collisionBox.height,
+                        RED
+                    );
+                }
+
+                //--Draw seed bag collision--
+                Rectangle seedCol = seed.GetCollider();
+
+                DrawRectangleLines(
+                    (int)seedCol.x,
+                    (int)seedCol.y,
+                    (int)seedCol.width,
+                    (int)seedCol.height,
+                    RED
+                );
+
+                //--Draw FPS--
+                DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, BLACK);
+
+                //--Draw pot counter--
+                DrawText(TextFormat("Pots: %i", potManager.pots.size()), 10, 30, 20, BLACK);
+
+                //--Draw bee counter--
+                DrawText(TextFormat("Bees: %i", beeManager.bees.size()), 10, 50, 20, BLACK);
+
+                //--Draw time--
+                DrawText(TextFormat("Num bees passed: %i", beeManager.BeesPassed()), 10, 70, 20, BLACK);
+            }
+        }
+        else if (gameState == GAME_OVER)
+        {
+            ClearBackground(BLACK);
+
+            int textWidth = MeasureText("GAME OVER", 40);
+            int textStartX = screenWidth / 2 - textWidth / 2;
+            int textStartY = screenHeight / 2 - 20;
+            DrawText("GAME OVER", textStartX, screenHeight / 2 - 20, 40, RED);
+
+            textWidth = MeasureText("Press R to Restart", 20);
+            textStartX = screenWidth / 2 - textWidth / 2;
+            textStartY = screenHeight / 2 - 10;
+            DrawText("Press R to Restart", textStartX, screenHeight / 2 + 40, 20, RED);
+
+            if (IsKeyPressed(KEY_R))
+            {
+                ResetGame();
+            }
         }
 
         EndDrawing();
     }
 
+    //--Unload all assets--
     for (auto &bees : beeManager.bees)
     {
         bees.Unload();
     }
 
     potManager.Unload();
-
     player.Unload();
     seed.Unload();
     UnloadTexture(bg);
-    CloseWindow();
 
+    CloseWindow();
     return 0;
 }
